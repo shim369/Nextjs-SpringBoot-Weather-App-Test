83412bcd276dc6c6cbf10e10d93813b27fa36497
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2023-10-22 16:12:31 +0900

    update function

---
+++ backend/src/main/java/com/example/backend/WeatherData.java
@@ -0,0 +1,22 @@
+package com.example.backend;
+
+public class WeatherData {
+    private String description;
+    private double temperature;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+}
---
+++ backend/src/main/java/com/example/backend/WeatherService.java
@@ -0,0 +1,26 @@
+package com.example.backend;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class WeatherService {
+    
+    @Value("${openweather.api.key}")
+    private String openWeatherApiKey;
+    
+    private final String openWeatherApiUrl = "http://api.openweathermap.org/data/2.5/forecast";
+
+    private final RestTemplate restTemplate;
+
+    public WeatherService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public WeatherData getWeatherData(String city) {
+        String url = openWeatherApiUrl + "?q=" + city + "&appid=" + openWeatherApiKey;
+        WeatherData response = restTemplate.getForObject(url, WeatherData.class);
+        return response;
+    }    
+}
